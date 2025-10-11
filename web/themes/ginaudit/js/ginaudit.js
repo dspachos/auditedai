1fb42f8da3b919bdd8e6b661bff5c57cdf0a8cdd
Dimitris Spachos <dimitris.spachos@amazeelabs.com>
2025-10-11 10:29:46 +0300

    feat(theme): add GinAudit theme with styles, scripts, and templates

---
+++ web/themes/ginaudit/js/ginaudit.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * GinAudit behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ginaudit = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
